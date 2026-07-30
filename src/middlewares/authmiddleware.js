df483cfc287b8e4ce5e3ce56cbab1e028bca076b
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-07-30 19:07:42 -0300

    se creo el middleware de autenticador

---
+++ src/middlewares/authmiddleware.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+
+export const autenticador = (req, res, next) => {
+  try {
+    const token = req.cookies.cookieToken;
+    if (!token) {
+      return res
+        .status(401)
+        .json({ mensaje: "Acceso no autorizado, token faltante." });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(403).json({ mensaje: "Tocken expirado o invalido" });
+  }
+};
+export const esAdmin = (req, res, next) => {
+  //verificar si tenemos los datos del usuario en el req y verificar si es admin
+
+  if (!req.user || req.user.rol !== "Admin") {
+    return res
+      .status(403)
+      .json({ mensaje: "Acceso denegado: permisos insuficientes" });
+  }
+
+  next();
+};
