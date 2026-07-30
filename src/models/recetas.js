7ebc1261df3800f2a3da962da59f4f8fa38bc4f4
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-07-30 18:21:53 -0300

    se creo la carpeta controllers con receta y usuario

--- src/models/recetas.js
+++ src/models/recetas.js
@@ -1,40 +1,37 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const RecetaSchema = new Schema(
-{
-    //propiedadd del objeto Receta
-    nombreReceta:{
-type: String, 
-required: true,
-unique: true,
-minlength:5,
-maxlength:100,
-trim: true,
+  {
+      nombreReceta: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 5,
+      maxlength: 100,
+      trim: true,
     },
-    
-    imagen:{
-        type: String,
-        required: true,
-        validate: (valor) =>  /^https:\/\/.+\.(jpg|jpeg|png|webp|avif|svg)$/.test(valor)
-        
-    },
-    categoria:{
-        type: Schema.Types.ObjectId, 
-        required: true,
-        ref: "categoria",
-    },
-    descripcion: {
-        type: String,
-        minlength: 10,
-        maxlength:500,
-        required: true,
-    },
-    
-},
-{
-    timestamps: true, //tengo la fecha y hora de creacion y actualizacion
-}
 
+    imagen: {
+      type: String,
+      required: true,
+      validate: (valor) =>
+        /^https:\/\/.+\.(jpg|jpeg|png|webp|avif|svg)$/.test(valor),
+    },
+    categoriaRecet: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "categoria",
+    },
+    descripcionRecet: {
+      type: String,
+      minlength: 10,
+      maxlength: 500,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, //tengo la fecha y hora de creacion y actualizacion
+  },
 );
-const Receta = mongoose.model('Receta', RecetaSchema)
-export default Receta
+const Receta = mongoose.model("Receta", RecetaSchema);
+export default Receta;
