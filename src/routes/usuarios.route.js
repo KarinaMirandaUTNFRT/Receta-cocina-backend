b725069f1dc51c4dfd0d4c6a5f0b9ab94819a6e3
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-07-31 10:46:14 -0300

    se agregaron rutas de logout

--- src/routes/usuarios.route.js
+++ src/routes/usuarios.route.js
@@ -1,18 +1,18 @@
-import { Router } from 'express';
-import { 
-    registrarUsuario,
-    login,
-verificarCuenta,
-solicitarNuevoCodigo
-} from '../controllers/usuario.js';
-
+import { Router } from "express";
+import {
+  registrarUsuario,
+  login,
+  verificarCuenta,
+  solicitarNuevoCodigo,
+  logout
+} from "../controllers/usuario.js";
 
 const router = Router();
 
 // Endpoint para crear usuario (POST /usuarios)
-router.post('/', registrarUsuario);
-router.post('/login', login);
-router.post('/verificar-cuenta', verificarCuenta);
-router.post('/nuevo-codigo', solicitarNuevoCodigo);
-
+router.post("/", registrarUsuario);
+router.post("/login", login);
+router.post("/verificar-cuenta", verificarCuenta);
+router.post("/nuevo-codigo", solicitarNuevoCodigo);
+router.post("/logout", logout);
 export default router;
